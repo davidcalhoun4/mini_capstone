e2b75425aca182694dd6fcd930a2321714a40988
david <calhoun5321@gmail.com>
2021-01-18 12:25:46 -0800

    adds update action

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -17,7 +17,23 @@ class Api::ProductsController < ApplicationController
       description: params[:description]
     )
     @product.save
-    render "create.json.jb"
+    render "show.json.jb"
+  end
+
+  def update
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.description = params[:description] || @product.description
+
+    @product.save
+    render "show.json.jb"
+  
   end
   
 end
+
+
+# note you can point create and update controller action to show.json.jb
